8253fa487bf9f81c61c119b1dad9bc11e34f4798
Mae5tr0 <i.kirill.maestro@gmail.com>
2018-01-18 12:28:16 +0700

    Add files for '8 Puzzle' assigment

---
+++ 8-puzzle/Board.java
@@ -0,0 +1,65 @@
+public class Board {
+    private final int n;
+    private final int[][] blocks;
+    private final int[][] tiles;
+
+    // construct a board from an n-by-n array of blocks
+    // (where blocks[i][j] = block in row i, column j)
+    public Board(int[][] blocks) {
+        n = blocks.length;
+        this.blocks = blocks;
+
+        tiles = new int[n][n];
+    }
+
+    // board dimension n
+    public int dimension() {
+        return n;
+    }
+
+    // number of blocks out of place
+    public int hamming() {
+        return 0;
+    }
+
+    // sum of Manhattan distances between blocks and goal
+    public int manhattan() {
+        return 0;
+    }
+    // is this board the goal board?
+    public boolean isGoal() {
+        return false;
+    }
+
+    // a board that is obtained by exchanging any pair of blocks
+    public Board twin() {
+        return new Board(new int[0][0]);
+    }
+    // does this board equal y?
+    public boolean equals(Object y) {
+        return false;
+    }
+
+    // all neighboring boards
+    public Iterable<Board> neighbors() {
+        return null;
+    }
+
+    // string representation of this board (in the output format specified below)
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(n + "\n");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                s.append(String.format("%2d ", tiles[i][j]));
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
+
+    // unit tests (not graded)
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 8-puzzle/Solver.java
@@ -0,0 +1,28 @@
+public class Solver {
+    private final Board initial;
+
+    // find a solution to the initial board (using the A* algorithm)
+    public Solver(Board initial) {
+        this.initial = initial;
+    }
+
+    // is the initial board solvable?
+    public boolean isSolvable() {
+        return false;
+    }
+
+    // min number of moves to solve initial board; -1 if unsolvable
+    public int moves() {
+        return 0;
+    }
+
+    // sequence of boards in a shortest solution; null if unsolvable
+    public Iterable<Board> solution() {
+        return null;
+    }
+
+    // solve a slider puzzle (given below)
+    public static void main(String[] args) {
+
+    }
+}
