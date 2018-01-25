b82fe5a1d19b84010ef197f5bba071b938198eec
Mae5tr0 <i.kirill.maestro@gmail.com>
2018-01-25 17:16:46 +0700

    Implement Brute algoritm

--- kd-trees/KdTree.java
+++ kd-trees/KdTree.java
@@ -3,6 +3,8 @@ import edu.princeton.cs.algs4.RectHV;
 import java.util.ArrayList;
 
 public class KdTree {
+    private int length;
+
     private static class Node {
         private Point2D p;      // the point
         private RectHV rect;    // the axis-aligned rectangle corresponding to this node
@@ -12,39 +14,56 @@ public class KdTree {
 
     // construct an empty set of points
     public KdTree() {
+        length = 0;
 
     }
 
     // is the set empty?
     public boolean isEmpty() {
-        return false;
+        return length == 0;
     }
 
     // number of points in the set
     public int size() {
-        return 0;
+        return length;
     }
 
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
+        if (p == null) throw new java.lang.IllegalArgumentException();
+
+        length++;
+
+
+    }
+
+    private void addNode() {
 
     }
 
     // does the set contain point p?
     public boolean contains(Point2D p) {
+        if (p == null) throw new java.lang.IllegalArgumentException();
+
         return false;
     }
 
     // draw all points to standard draw
-    public void draw() {}
+    public void draw() {
+        
+    }
 
     // all points that are inside the rectangle (or on the boundary)
     public Iterable<Point2D> range(RectHV rect)  {
+        if (rect == null) throw new java.lang.IllegalArgumentException();
+
         return new ArrayList<>();
     }
 
     // a nearest neighbor in the set to point p; null if the set is empty
     public Point2D nearest(Point2D p) {
+        if (p == null) throw new java.lang.IllegalArgumentException();
+
         return new Point2D(0, 0);
     }
 
--- kd-trees/PointSET.java
+++ kd-trees/PointSET.java
@@ -1,49 +1,121 @@
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.StdDraw;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class PointSET {
+    Set<Point2D> points;
+
     // construct an empty set of points
     public PointSET() {
-
+        points = new TreeSet<>();
     }
 
     // is the set empty?
     public boolean isEmpty() {
-        return false;
+        return points.isEmpty();
     }
 
     // number of points in the set
     public int size() {
-        return 0;
+        return points.size();
     }
 
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
+        if (p == null) throw new java.lang.IllegalArgumentException();
 
+        points.add(p);
     }
 
     // does the set contain point p?
     public boolean contains(Point2D p) {
-        return false;
+        if (p == null) throw new java.lang.IllegalArgumentException();
+
+        return points.contains(p);
     }
 
     // draw all points to standard draw
-    public void draw() {}
+    public void draw() {
+        for (Point2D p : points) {
+            p.draw();
+        }
+    }
 
     // all points that are inside the rectangle (or on the boundary)
     public Iterable<Point2D> range(RectHV rect)  {
-        return new ArrayList<>();
+        if (rect == null) throw new java.lang.IllegalArgumentException();
+
+        List<Point2D> result = new ArrayList<>();
+
+        for (Point2D p : points) {
+            if (rect.contains(p)) result.add(p);
+        }
+
+        return result;
     }
 
     // a nearest neighbor in the set to point p; null if the set is empty
     public Point2D nearest(Point2D p) {
-        return new Point2D(0, 0);
+        if (p == null) throw new java.lang.IllegalArgumentException();
+        if (isEmpty()) return null;
+
+        double minimumDistance = Double.POSITIVE_INFINITY;
+        Point2D nearest = p;
+
+        for (Point2D candidate : points) {
+            double distance = candidate.distanceTo(p);
+
+            if (distance < minimumDistance) {
+                minimumDistance = distance;
+                nearest = candidate;
+            }
+        }
+
+        return nearest;
     }
 
     // unit testing of the methods (optional)
     public static void main(String[] args) {
+        StdDraw.setPenRadius(0.01);
 
+        List<Point2D> points = new ArrayList<>();
+        points.add(new Point2D(0.372, 0.497));
+        points.add(new Point2D(0.564, 0.413));
+        points.add(new Point2D(0.226, 0.577));
+        points.add(new Point2D(0.144, 0.179));
+        points.add(new Point2D(0.083, 0.510));
+        points.add(new Point2D(0.320, 0.708));
+        points.add(new Point2D(0.417, 0.362));
+        points.add(new Point2D(0.862, 0.825));
+        points.add(new Point2D(0.785, 0.725));
+        points.add(new Point2D(0.499, 0.208));
+
+        PointSET set = new PointSET();
+        for (Point2D p : points) {
+            set.insert(p);
+        }
+
+        set.draw();
+
+        RectHV rect = new RectHV(0.144, 0.179, 0.372, 0.497);
+        rect.draw();
+
+        StdDraw.setPenColor(StdDraw.RED);
+        for (Point2D interseptionPoint : set.range(rect)) {
+            interseptionPoint.draw();
+        }
+
+        Point2D testPoint = new Point2D(0.550, 0.400);
+        StdDraw.setPenColor(StdDraw.ORANGE);
+        testPoint.draw();
+
+        Point2D nearest = set.nearest(testPoint);
+        StdDraw.setPenColor(StdDraw.GREEN);
+        nearest.draw();
     }
 }
